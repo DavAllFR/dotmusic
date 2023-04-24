5858b612b5ee8923c37dcf138f9bac1fac99b923
DavAll <davall2705@gmail.com>
2023-04-24 21:43:11 +0200

    Addition of the index page

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+
+const server = fastify();
+
+server.register(fastifyStatic, {
+    root: path.join(__dirname, "views"),
+});
+server.register(fastifyStatic, {    
+    root: path.join(__dirname, "assets"),
+    prefix: "/assets/",
+    decorateReply: false,
+});
+
+server.get("/", async (request, reply) => {
+    return reply.sendFile("index.html");
+});
+
+server.listen({port:3000}, (err, address) => {
+    if (err) throw err;
+    console.log(`Server listening at ${address}`);
+});
