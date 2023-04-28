a4f3e82f861b61ffc3fef359e1abb5b7e1ce287c
DavAll <davall2705@gmail.com>
2023-04-29 00:00:52 +0200

    Addition of socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
+import fastifySocketIO from "fastify-socket.io";
 import path from "path";
 
 const server = fastify();
@@ -8,10 +9,11 @@ server.register(fastifyStatic, {
     root: path.join(__dirname, "views"),
 });
 server.register(fastifyStatic, {    
-    root: path.join(__dirname, "assets"),
+    root: path.join(__dirname, "..", "assets"),
     prefix: "/assets/",
     decorateReply: false,
 });
+server.register(fastifySocketIO, {});
 
 server.get("/", async (request, reply) => {
     return reply.sendFile("index.html");
@@ -20,4 +22,11 @@ server.get("/", async (request, reply) => {
 server.listen({port:3000,host:"127.0.0.1"}, (err, address) => {
     if (err) throw err;
     console.log(`Server listening at ${address}`);
+    server.io.on("connection", (socket) => {
+        console.log("Client connected");
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
+        
+    });
 });
